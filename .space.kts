d01e63680a9eb30ff9ebe2f4f1578d143b79c83d
Марк Сорокин <s1211061167@stu.lipetsk.ru>
2022-10-25 13:38:39 +0000

    CI: .space.kts file

---
+++ .space.kts
@@ -0,0 +1,18 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Basic setup") {
+    container(image = "flumine.registry.jetbrains.space/p/flumine/docker-images/ansible:0.1.0") {
+    	shellScript {
+			shellScript {
+            	content = """
+                	apt-get install -y nodejs npm
+                    npm i --dev
+                """
+            }
+        }
+    }
+}
